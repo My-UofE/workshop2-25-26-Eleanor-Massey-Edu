97712ae638b9c134b42f472d5ce3825f0a7ecf0a
Eleanor Massey <erm242@exeter.ac.uk>
2026-01-28 11:53:37 +0000

    Done task 1

--- ecm1410-workshop-2-main/HelloWorldApp.java
+++ ecm1410-workshop-2-main/HelloWorldApp.java
@@ -1,7 +1,13 @@
 // Add your code below
 public class HelloWorldApp{
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        System.out.print("Hello World! ");
+        System.out.print("My name is ");
+        System.out.print(args[0]);
+        System.out.print(".");
+        System.out.print(" I am ");
+        System.out.print(args[1]);
+        System.out.println(" years old.");
     }
 }
 
